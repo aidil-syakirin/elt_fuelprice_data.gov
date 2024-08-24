65644d576d8846ec62522aeac76627135591b4ad
aidil-syakirin <106723859+aidil-syakirin@users.noreply.github.com>
2024-08-24 22:33:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 from src.extract import create_and_extract_data
 from src.transform import transform_and_remove_duplicate
+from src.update import update_values
 
 from dotenv import load_dotenv
 load_dotenv() #take environment variables from .env only for local testing
@@ -16,7 +17,10 @@ password = os.getenv("password")
 
 #extract and transfroming data will be done in postgresql so we just call the function
 
+#to establish the baseline for the fuel price table
 create_and_extract_data()
 
+#to remove unneeded column and duplicate
 transform_and_remove_duplicate()
+
 
