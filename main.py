8a80b04dd6c06c4c8456d41084266625b84749aa
aidil-syakirin <106723859+aidil-syakirin@users.noreply.github.com>
2024-08-29 14:23:40 +0800

    Update main.py
    Change the code process

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 from src.extract import create_and_extract_data
 from src.transform import transform_and_remove_duplicate
+from src.extract import connect_to_postgress
 from src.update import update_values
 
 from dotenv import load_dotenv
@@ -15,12 +16,25 @@ port = os.getenv("port")
 user = os.getenv("user")
 password = os.getenv("password")
 
+#this creates a variable that the script execution start time
+start_time = datetime.now()
+
+# make a connection to postgres and insert data from csv file in local env
+print("\nExtracting and transforming data in sql...")
+create_and_extract_data(dbname, host, port, user, password)
+print('\nExtraction and transformation in sql completed')
+
+# remove duplicated data and removing unneeded column
+print("\nRemoving duplicated data...")
+transform_and_remove_duplicate(dbname, host, port, user, password)
+print('\nDuplicated data removed')
+
+# this creates a variable that calculates how long it takes to run the script
+execution_time = datetime.now() - start_time
+print(f"\nTotal execution time (hh:mm:ss.ms) {execution_time}")
 #extract and transfroming data will be done in postgresql so we just call the function
 
-#to establish the baseline for the fuel price table
-create_and_extract_data()
-
 #to remove unneeded column and duplicate
-transform_and_remove_duplicate()
+#transform_and_remove_duplicate()
 
 
