650b8f4b9e5ceb0d301e849b047919f0183a7438
aidil-syakirin <106723859+aidil-syakirin@users.noreply.github.com>
2024-08-29 15:01:47 +0800

    Update transform.py
    Debug on creating the duplicate table.

--- src/transform.py
+++ src/transform.py
@@ -1,7 +1,7 @@
 
 from extract import connect_to_postgres 
 
-def transform_and_remove_duplicate():
+def transform_and_remove_duplicate(dbname, user, password, host, port):
 
     #postgresql configuration
     conn = connect_to_postgres(dbname, user, password, host, port)
@@ -10,7 +10,7 @@ def transform_and_remove_duplicate():
     cursor = conn.cursor()
 
     #creating a table with level dt_types only as there is 2 data entry for specific date
-    cursor.execute('CREATE TABLE tr_fuelprice AS (SELECT * FROM fuelprice WHEN dt_type = 'level');')
+    cursor.execute('''CREATE TABLE tr_fuelprice AS (SELECT * FROM fuelprice WHERE dt_type = 'level');''')
 
     #remove dt_type column as we no longer need for analysis
     cursor.execute('ALTER TABLE tr_fuelprice DROP COLUMN dt_type')
